c3e0c889a463e94569b74019330dfa27397adfbb
soumyo19 <fsoummya@gmail.com>
2022-05-18 10:27:49 +0600

    venus: build with Lawnchair

--- spark_venus.mk
+++ spark_venus.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from venus device
 $(call inherit-product, device/xiaomi/venus/device.mk)
 
+# Lawnchair
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+
 # Inherit some common Aosp stuff.
 $(call inherit-product, vendor/spark/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
