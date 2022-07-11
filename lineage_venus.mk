a3a9c4afe313820cc0370a5300206262e79cfaef
kota <kotaroo2207@gmail.com>
2022-07-11 23:00:34 +0200

    venus: Include AnxVerse.
    AnxVerse = Next-Generation AnxCamera.

--- lineage_venus.mk
+++ lineage_venus.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/venus/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit AnxVerse Setup.
+$(call inherit-product, vendor/xiaomi/venus-miuicamera/products/miuicamera.mk)
+
 # Rom flags
 RICE_MAINTAINER := Soumyo•Tofu
 RICE_DEVICE := Mi 11
